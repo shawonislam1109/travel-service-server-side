386b290d150f961797c1cf26f3903e280865de8b
shawonislam1109 <shawonislam1109@gmail.com>
2022-11-08 21:54:47 +0600

    pagination

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express')
 const cors = require('cors')
-require('dotenv').config
+require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000;
 
@@ -10,8 +10,6 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 
-// username : travelUser
-//  password : o483NanrYboHxTc8
 
 const user = process.env.travel_user;
 const password = process.env.travel_password;
@@ -22,15 +20,25 @@ app.get('/', (req, res) => {
     res.send('this is service review connected')
 })
 
-const uri = `mongodb+srv://travelUser:o483NanrYboHxTc8@cluster0.5rnuhbi.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri)
+const uri = `mongodb+srv://${user}:${password}@cluster0.5rnuhbi.mongodb.net/?retryWrites=true&w=majority`;
+// console.log(uri)
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
+    const serviceCollection = client.db("TouristAllData").collection('serviceData')
     try {
-
+        app.get('/services', async (req, res) => {
+            const page = parseInt(req.query.page);
+            const size = parseInt(req.query.size);
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const service = await cursor.skip(page * size).limit(size).toArray();
+            const count = await serviceCollection.estimatedDocumentCount()
+            res.send({ count, service })
+        })
     }
+
     finally {
 
     }
